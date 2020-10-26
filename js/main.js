80a433f345fdfcc84bbbf768a5b8e9e45eb866e3
Rastsislau Krol <lorkmun@outlook.com>
2020-10-26 23:27:10 +0200

    Restyling sait

---
+++ js/main.js
@@ -0,0 +1,92 @@
+
+
+/*MOBILE NAV */
+$(document).ready(function () {
+  /*STICKY NAVIGATION */
+  $(".js--dates-section").waypoint(
+    function (direction) {
+      if (direction == "down") {
+        $("nav").addClass("sticky");
+      } else {
+        $("nav").removeClass("sticky");
+      }
+    },
+    {
+      offset: "40px;",
+    }
+  );
+
+  /*NAVIGATION SCROLL */
+  $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function (event) {
+      // On-page links
+      if (
+        location.pathname.replace(/^\//, "") ==
+          this.pathname.replace(/^\//, "") &&
+        location.hostname == this.hostname
+      ) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length
+          ? target
+          : $("[name=" + this.hash.slice(1) + "]");
+        // Does a scroll target exist?
+        if (target.length) {
+          // Only prevent default if animation is actually gonna happen
+          event.preventDefault();
+          $("html, body").animate(
+            {
+              scrollTop: target.offset().top,
+            },
+            1000,
+            function () {
+              // Callback after animation
+              // Must change focus!
+              var $target = $(target);
+              $target.focus();
+              if ($target.is(":focus")) {
+                // Checking if the target was focused
+                return false;
+              } else {
+                $target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
+                $target.focus(); // Set focus again
+              }
+            }
+          );
+        }
+      }
+    });
+
+  $(".js--mob-nav-icon").click(function () {
+    var nav = $(".js--main-nav");
+    var icon = $(".mob-menu-icon");
+
+    if (icon.attr("name") == "menu-outline") {
+      icon.attr("name", "close-outline");
+    } else {
+      icon.attr("name", "menu-outline");
+    }
+
+    nav.slideToggle(200);
+  });
+})
+
+
+const selected = document.querySelectorAll('.dates');
+for( let i = 0; i < selected.length; i++ ) {
+  selected[i].addEventListener('click', function(e) {
+    if (!e.target.classList.contains("selected")) {
+      for(let i = 0; i < selected.length; i++) {
+        if(selected[i].classList.contains("selected")) {
+          selected[i].classList.remove("selected");
+        }
+      }
+      e.target.classList.add("selected");
+    }
+  })
+  
+}
+// console.log();
